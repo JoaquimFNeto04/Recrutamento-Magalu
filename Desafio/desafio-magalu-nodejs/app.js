a5e8f8776560dd5c8a0884e85ff506b964c9a4c8
Joaquim Firmino Neto <31675094+JoaquimFNeto04@users.noreply.github.com>
2023-10-03 11:28:06 -0300

    Endpoints Funcionando
    Foi criado os endpoints  usando node.js usando express

--- Desafio/desafio-magalu-nodejs/app.js
+++ Desafio/desafio-magalu-nodejs/app.js
@@ -1,13 +1,53 @@
+var request = require('request');
 const express = require('express')
 const app = express()
 const port = 3000
 
+const apiUrl = 'api.integracommerce.com.br'
+const apiKey = '72e148a7-35bc-41ac-b968-0101cd21bdc9'
+const clientId = '2OZPBrxw5aLXNDZUdoVIal4CO87jfls47xbSYn618yQ'
+const userName = 'xsellerntcst50xapi'
+const password = 'SBinB8SGxRuLHhBlrpPQ'
+
+
+// Inicializar o endpoint OAuth2 usando apiUrl
 app.post('/', (req, res) => {
-  res.send('Olá Mundo post!!')
+
+  console.log("Endpoint de Iniciação foi chamado com sucesso.")
+
+  var options = {
+    'method': 'POST',
+    'url': 'https://api.integracommerce.com.br/api/v1/oauth2/rollout/initialize',
+    'headers': {
+      'x-api-key': '72e148a7-35bc-41ac-b968-0101cd21bdc9',
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic eHNlbGxlcm50Y3N0NTB4YXBpOlNCaW5COFNHeFJ1TEhoQmxycFBR'
+    },
+    body: JSON.stringify({
+      "client_id": "2OZPBrxw5aLXNDZUdoVIal4CO87jfls47xbSYn618yQ"
+    })
+
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    res.send(response.body)
+  })
 })
 
 app.get('/', (req, res) => {
-  res.send('Olá Mundo!!')
+  console.log("Endpoint de Status foi chamado com sucesso.")
+  var options = {
+    'method': 'GET',
+    'url': 'https://api.integracommerce.com.br/api/v1/oauth2/callback/status?clientId=2OZPBrxw5aLXNDZUdoVIal4CO87jfls47xbSYn618yQ&username=xsellerntcst50xapi',
+    'headers': {
+      'x-api-key': '72e148a7-35bc-41ac-b968-0101cd21bdc9',
+      'Authorization': 'Basic eHNlbGxlcm50Y3N0NTB4YXBpOlNCaW5COFNHeFJ1TEhoQmxycFBR'
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.send(response.body);
+  });
 })
 
 app.listen(port, () => {
