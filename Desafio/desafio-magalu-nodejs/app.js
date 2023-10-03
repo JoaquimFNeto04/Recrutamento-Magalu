4411b88f7a0fba54e46458e10d8d665649553ec4
Joaquim Firmino Neto <31675094+JoaquimFNeto04@users.noreply.github.com>
2023-10-03 12:08:03 -0300

    Perfumaria

--- Desafio/desafio-magalu-nodejs/app.js
+++ Desafio/desafio-magalu-nodejs/app.js
@@ -3,19 +3,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const apiUrl = 'api.integracommerce.com.br'
-const apiKey = '72e148a7-35bc-41ac-b968-0101cd21bdc9'
-const clientId = '2OZPBrxw5aLXNDZUdoVIal4CO87jfls47xbSYn618yQ'
-const userName = 'xsellerntcst50xapi'
-const password = 'SBinB8SGxRuLHhBlrpPQ'
+// const apiUrl = 'api.integracommerce.com.br'
+// const apiKey = '72e148a7-35bc-41ac-b968-0101cd21bdc9'
+// const clientId = '2OZPBrxw5aLXNDZUdoVIal4CO87jfls47xbSYn618yQ'
+// const userName = 'xsellerntcst50xapi'
+// const password = 'SBinB8SGxRuLHhBlrpPQ'
 
 
-// Inicializar o endpoint OAuth2 usando apiUrl
+// Inicializa o endpoint OAuth2
 app.post('/', (req, res) => {
 
   console.log("Endpoint de Iniciação foi chamado com sucesso.")
 
-  var options = {
+  const options = {
     'method': 'POST',
     'url': 'https://api.integracommerce.com.br/api/v1/oauth2/rollout/initialize',
     'headers': {
@@ -34,9 +34,12 @@ app.post('/', (req, res) => {
   })
 })
 
+// Inicializa o endpoint verificação de Status
 app.get('/', (req, res) => {
+
   console.log("Endpoint de Status foi chamado com sucesso.")
-  var options = {
+
+  const options = {
     'method': 'GET',
     'url': 'https://api.integracommerce.com.br/api/v1/oauth2/callback/status?clientId=2OZPBrxw5aLXNDZUdoVIal4CO87jfls47xbSYn618yQ&username=xsellerntcst50xapi',
     'headers': {
@@ -44,6 +47,7 @@ app.get('/', (req, res) => {
       'Authorization': 'Basic eHNlbGxlcm50Y3N0NTB4YXBpOlNCaW5COFNHeFJ1TEhoQmxycFBR'
     }
   };
+
   request(options, function (error, response) {
     if (error) throw new Error(error);
     res.send(response.body);
